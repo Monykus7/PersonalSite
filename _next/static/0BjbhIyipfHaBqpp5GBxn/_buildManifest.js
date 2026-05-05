4b665e57b6b6656cd76ce73687a4056a15674d90
Monykus7 <adityamahesh16@gmail.com>
2026-05-05 17:58:19 -0400

    Updates

---
+++ _next/static/0BjbhIyipfHaBqpp5GBxn/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/PersonalSite//_next/:path+",
+        "destination": "/PersonalSite/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
